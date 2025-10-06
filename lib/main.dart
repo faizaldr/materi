dbcae35e28aeb3955cbb38023ee71224ba9ecb54
faizaldr <fdr00zenc@gmail.com>
2025-10-06 13:33:16 +0700

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-void main(){
+void main() {
   var app = App();
   runApp(app);
 }
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage()
-    );
+    return MaterialApp(home: HomePage(), );
   }
 }
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _HomePageState();
   }
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
+  var n = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Selamat Datang"),),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              n++;
+            });
+          },
+          child: Text("Angka : ${n}"),
+        ),
+      ),
     );
   }
 }
-
