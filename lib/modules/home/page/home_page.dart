f99274eee3c70d16e9619e10d97e2969b572b5ae
faizaldr <fdr00zenc@gmail.com>
2025-10-07 14:24:32 +0700

    o

--- lib/modules/home/page/home_page.dart
+++ lib/modules/home/page/home_page.dart
@@ -17,6 +17,32 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(title: Text(text)),
       body: Center(),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("contoh"),
+              accountEmail: Text("email"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("F", style: TextStyle(fontSize: 24)),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("HOME"),
+              onTap: () => Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (context) => HomePage())),
+            ),
+             ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("LOGOUT"),
+              onTap: (){}
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
