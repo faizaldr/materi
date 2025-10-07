bf59e1487898a1fb807d1118117fc35742988c47
faizaldr <fdr00zenc@gmail.com>
2025-10-07 15:21:26 +0700

    o

---
+++ lib/utils/secure_storage_utils.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageUtils {
+  static final FlutterSecureStorage _storage = FlutterSecureStorage();
+
+  static AndroidOptions _getAndroidOptions() =>
+      const AndroidOptions(encryptedSharedPreferences: true);
+
+  static IOSOptions _getIosOptions() =>
+      const IOSOptions(accessibility: KeychainAccessibility.first_unlock);
+
+  static Future<void> saveData({
+    required String key,
+    required String value,
+  }) async {
+    await _storage.write(
+      key: key,
+      value: value,
+      aOptions: _getAndroidOptions(),
+      iOptions: _getIosOptions(),
+    );
+  }
+  
+  static Future<void> readData({
+    required String key,
+    required String value,
+  }) async {
+    await _storage.read(
+      key: key,
+      aOptions: _getAndroidOptions(),
+      iOptions: _getIosOptions(),
+    );
+  }
+
+  
+  static Future<void> deleteAllData({
+    required String key,
+    required String value,
+  }) async {
+    await _storage.deleteAll(
+      aOptions: _getAndroidOptions(),
+      iOptions: _getIosOptions(),
+    );
+  }
+}
