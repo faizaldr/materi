480278556a5368875f97db9066eb578cf24be7f0
faizaldr <fdr00zenc@gmail.com>
2025-10-09 10:44:30 +0700

    l

--- lib/modules/address/pages/address_page.dart
+++ lib/modules/address/pages/address_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:materi/modules/address/data/address_service.dart';
 import 'package:materi/modules/address/models/address_model.dart';
+import 'package:materi/utils/message.dart';
 
 class AddressPage extends StatefulWidget {
   @override
@@ -11,20 +12,36 @@ class AddressPage extends StatefulWidget {
 
 class _AddressPageState extends State<AddressPage> {
   int _indexPage = 1;
-  Future<Data?>? _future;
+  int _maxIndexPage = 1;
+  List<Data>? _addressList = [];
 
   @override
   void initState() {
     super.initState();
+    _initData();
   }
 
-  void _initData() {
-    setState(() {
-      _future = actionGetAddressService(_indexPage);
-    });
+  void _initData() async {
+    final result = await actionGetAddressService(_indexPage);
+    if (result != null)
+      setState(() {
+        _addressList!.addAll(result.data!.toList());
+        _maxIndexPage = result.meta!.pagination!.pageSize!;
+      });
   }
 
-  void _
+  void _appendData(context) async {
+    if (_indexPage < _maxIndexPage) {
+      _indexPage++;
+      final result = await actionGetAddressService(_indexPage);
+      if (result != null)
+        setState(() {
+          _addressList!.addAll(result.data!.toList());
+        });
+    } else {
+      Message.errorMessage(context, "Data sudah habis");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
