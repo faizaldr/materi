489d87741d0524b374a0dea0f5f0fbbf4af7c9cb
faizaldr <fdr00zenc@gmail.com>
2025-10-09 09:46:49 +0700

    ok

--- lib/utils/url_list.dart
+++ lib/utils/url_list.dart
@@ -1,3 +1,4 @@
 const BASE_URL = "http://172.19.0.1:1337/api";
 const LOGIN_URL = BASE_URL + "/auth/local";
 const UPDATE_ACCOUNT_URL = BASE_URL + "/account/me";
+const ADDRESS_URL = BASE_URL + "/addresses";
