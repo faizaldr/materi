e91cae714d54ae16dd5c7214cc72cde619b14735
faizaldr <fdr00zenc@gmail.com>
2025-10-09 14:24:56 +0700

    k

--- lib/modules/address/pages/address_detail_page.dart
+++ lib/modules/address/pages/address_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:materi/component/text_form_component.dart';
 import 'package:materi/modules/address/models/address_model.dart';
 
 class AddressDetailPage extends StatefulWidget {
@@ -10,12 +11,94 @@ class AddressDetailPage extends StatefulWidget {
 }
 
 class _AddressDetailPageState extends State<AddressDetailPage> {
+  Data? data;
+  TextEditingController _addressController = TextEditingController();
+  TextEditingController _townController = TextEditingController();
+  String? _type;
+  double? _latitude;
+  double? _longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    data = widget.data;
+    if (data != null) {
+      _addressController.text = data!.address!;
+      _townController.text = data!.town!;
+      _type = data!.type;
+      _latitude = data!.latitude;
+      _longitude = data!.longitude;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [Text(widget.data != null ? "Tambah Data" : "Ubah Data")],
+      body: Container(
+        padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+        child: Form(
+          child: ListView(
+            children: [
+              SizedBox(height: 20),
+              Text(
+                widget.data == null ? "Tambah Data" : "Ubah Data",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              TextFormComponent(
+                Icons.share_location_rounded,
+                "Alamat anda",
+                "Alamat",
+                false,
+                _addressController,
+                TextInputType.name,
+              ),
+              SizedBox(height: 20),
+              TextFormComponent(
+                Icons.location_city,
+                "Kota anda",
+                "Kota",
+                false,
+                _townController,
+                TextInputType.name,
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.fromLTRB(10,0,10,0),
+                child: DropdownButton(
+                  hint: Text("Pilih Tipe Alamat"),
+                  items: _dropDownItem,
+                  value: _selectedValue,
+                  onChanged: _dropdownChange,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  border: BoxBorder.all(color: Colors.black, strokeAlign: 1),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.gps_fixed),
+                title: Text("${_latitude} - ${_longitude}"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  String? _selectedValue;
+
+  _dropdownChange(value) {
+    setState(() {
+      _selectedValue = value;
+    });
+  }
+
+  final List<DropdownMenuItem> _dropDownItem = [
+    DropdownMenuItem(child: Text("Rumah"), value: "Rumah"),
+    DropdownMenuItem(child: Text("Kos"), value: "Kos"),
+  ];
 }
